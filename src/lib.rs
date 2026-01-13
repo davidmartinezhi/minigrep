2ea865108c44849247c3ed81a869575d12abc2fb
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2026-01-12 20:10:27 -0500

    Refactor search functions to return iterators instead of vectors for improved performance and memory efficiency

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,15 @@
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = &'a str> + 'a {
+    contents.lines().filter(move |line| line.contains(query))
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(
+    query: &'a str,
+    contents: &'a str,
+) -> impl Iterator<Item = &'a str> + 'a {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(move |line| line.to_lowercase().contains(&query))
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,14 @@ use minigrep::search;
 use minigrep::search_case_insensitive;
 
 fn main() {
-    // Collect arfuments from command line
-    let args: Vec<String> = env::args().collect();
-
-    // Handling error when there's an error and returning config when success
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // Handling error when we only look for the error and nothing happens when success
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
@@ -26,10 +22,10 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+    let results: Vec<_> = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents).collect()
     } else {
-        search(&config.query, &contents)
+        search(&config.query, &contents).collect()
     };
 
     for line in results {
@@ -46,13 +42,17 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
